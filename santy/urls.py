0a530888554e2443907a4e5b182bc38b4cd2bace
L-SAN65 <alisanti.leo@gmail.com>
2026-08-21 16:48:39 -0500

    feat: redirect raiz / -> /login/ para Vercel

--- santy/urls.py
+++ santy/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="core:login", permanent=False)),
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("reservas/", include("reservations.urls")),
